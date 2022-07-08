5cd4fcfb6647df2d349e5c7aac614d7f6c188f35
MeowNeco <108581064+MeowNeco@users.noreply.github.com>
2022-07-08 22:24:40 +0900

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,1 +1,33 @@
-const ArgumentType=require
+const ArgumentType=require("https://github.com/LLK/scratch-vm/blob/develop/src/extension-support/argument-type.js");
+const BlockType=require("https://github.com/LLK/scratch-vm/blob/develop/src/extension-support/block-type.js");
+
+class Scratch3Hello{
+    constructor(runtime){
+        this.runtime=runtime;
+    }
+
+    getInfo () {
+        return{
+            id:"hello",
+            name:'hello',
+            blocks:[
+                {
+                    opecode:"showAlert",
+                    blockType:BlockType.COMMAND,
+                    text:"[TEXT]と表示する",
+                    arguments:{
+                        TEXT:{
+                        type:ArgumentType.STRING,
+                        defaultValue:"こんにちは"
+                        }
+                    } 
+                }
+            ]
+
+        };
+    }
+
+    showAlert(args){
+        alert(args.TEXT)
+    }
+}
